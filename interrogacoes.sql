3799de12e182947424e49a11a03374484b30ca8c
jessicaclcunha <102656765+jessicaclcunha@users.noreply.github.com>
2023-06-04 00:58:27 +0100

    some changes

--- interrogacoes.sql
+++ interrogacoes.sql
@@ -3,25 +3,76 @@ CREATE PROCEDURE totalPeso ()
 BEGIN
 	SELECT SUM(peso) AS total
 		FROM Uva;
-END $$
+END 
+$$
 
 DROP PROCEDURE totalPeso;
 CALL totalPeso();
 
 
 delimiter $$
-CREATE PROCEDURE totalQuantidade2022 ()
+CREATE PROCEDURE totalQuantidadeAno (Ano INT)
 BEGIN
 	SELECT SUM(quantidade) AS totalQuantidade
 		FROM Vinho
-    WHERE AnoProducao = 2010;    
+    WHERE AnoProducao = Ano;    
 END
 $$
 
-delimiter $$
-CREATE PROCEDURE todosResponsaveis ()
+DROP PROCEDURE totalQuantidadeAno;
+CALL totalQuantidadeAno (2021);
+
+DELIMITER $$
+CREATE PROCEDURE totalQuantidade ()
 BEGIN
-	SELECT nome AS todosResponsaveis
-		FROM Responsavel;
-END
+	SELECT SUM(quantidade) AS total
+		FROM Vinho;
+END $$
+
+DELIMITER $$
+CREATE PROCEDURE melhorQuinta ()
+BEGIN
+	SELECT id AS total
+		FROM Quinta
+        JOIN Vinho ON Vinho.id = Quinta.Vinho_id
+        GROUP BY id
+        ORDER BY SUM(Vinho.quantidade) DESC
+        LIMIT 1;
+END 
+$$
+
+DELIMITER $$
+CREATE PROCEDURE vinhoMaisProduzido ()
+BEGIN
+	SELECT Vinho.nome
+		FROM Vinho
+        GROUP BY Vinho.nome
+        ORDER BY sum(Vinho.quantidade) DESC
+        LIMIT 1;
+END 
+$$
+
+DELIMITER $$
+CREATE PROCEDURE zonaMaisProducao ()
+BEGIN
+	SELECT Zona.codigoGeo
+		FROM Zona
+        JOIN Quinta ON Quinta.Zona_codigoGeo = Zona.codigoGeo
+        JOIN Uva ON Quinta.Uva_id = Uva.id 
+        GROUP BY Zona.nome
+        ORDER BY sum(Uva.quantidade) DESC
+        LIMIT 1;
+END 
+$$
+
+DELIMITER $$
+CREATE PROCEDURE ResponsavelMaiorQuinta ()
+BEGIN
+	SELECT Responsavel.nome
+		FROM Responsavel
+        JOIN Quinta ON Quinta.Responsavel_nome = Responsavel.nome
+        GROUP BY Responsavel.nome
+        ORDER BY sum(Quinta.dimensao) DESC
+        LIMIT 1;
+END 
 $$
