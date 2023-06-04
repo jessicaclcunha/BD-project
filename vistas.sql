25e515e49b8a4fe9b7f54ded1ab49371be4c76b8
jessicaclcunha <102656765+jessicaclcunha@users.noreply.github.com>
2023-06-04 14:43:09 +0100

    adição das vistas

---
+++ vistas.sql
@@ -0,0 +1,10 @@
+CREATE VIEW viewQuinta AS
+SELECT id, dimensao, Zona_codigoGEO, VINHO_id, Responsavel_nome, Uva_id
+FROM Quinta;
+
+
+CREATE VIEW viewResponsaveis AS
+ SELECT Responsavel.nome, Quinta.id
+ FROM Responsavel
+ JOIN Quinta ON Responsavel.nome = Quinta.Responsavel_nome
+ ORDER BY Quinta.id;
