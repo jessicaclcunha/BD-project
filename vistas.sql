bcfec35777fd407858a1eda9efcffbab88dc0ace
Rodrigo Castro <118373977+luninss@users.noreply.github.com>
2023-06-06 13:48:59 +0100

    Update vistas.sql

--- vistas.sql
+++ vistas.sql
@@ -2,8 +2,12 @@ CREATE VIEW viewQuinta AS
 SELECT id, dimensao, Zona_codigoGEO, Vinho_id, Responsavel_nome, Uva_id
 FROM Quinta;
 
+SELECT * FROM viewQuinta;
+
 CREATE VIEW viewResponsaveis AS
  SELECT Responsavel.nome, Quinta.id
  FROM Responsavel
  JOIN Quinta ON Responsavel.nome = Quinta.Responsavel_nome
  ORDER BY Quinta.id;
+ 
+ SELECT * FROM viewResponsaveis;
